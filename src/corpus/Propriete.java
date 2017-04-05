da000293080ce1c87962aed39f8deeb72dcb729d
zhufangda <zhufangda@gmail.com>
2017-04-05 15:46:16 +0200

    增加常量类用于保存常用路径

---
+++ src/corpus/Propriete.java
@@ -0,0 +1,17 @@
+package corpus;
+
+import java.io.File;
+
+import com.sun.org.apache.bcel.internal.generic.StackInstruction;
+import com.sun.org.apache.regexp.internal.RESyntaxException;
+
+public class Propriete {
+	static public String PATH_RESOURCE = "res" + File.separator;
+	static public String PATH_ALPHABET = PATH_RESOURCE + "alphabet" + File.separator;
+	static public String PATH_CORPUS = PATH_RESOURCE + "corpus" + File.separator;
+	static public String PATH_CORPUS_EN = PATH_CORPUS + "anglais" + File.separator;
+	static public String PATH_CORPUS_FR = PATH_CORPUS + "francais" + File.separator;
+	static public String PATH_CORPUS_GE = PATH_CORPUS + "allemand" + File.separator;
+	static public String PATH_CORPUS_IT = PATH_CORPUS + "italien" + File.separator;
+	
+}
